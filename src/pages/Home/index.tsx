0c6f2f7c4807d5034cc24717d2aa33d8fcd90b2f
John Rodrigues <johnrodrigues97106@gmail.com>
2022-08-18 19:13:53 -0300

    Mudando title da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -40,19 +40,26 @@ export function Home() {
       minutesAmount: 0,
     },
   })
+
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
   useEffect(() => {
+    let interval: number
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
     }
+    return () => {
+      clearInterval(interval)
+    }
   }, [activeCycle])
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
+
     const newCycle: Cycle = {
       id,
       task: data.task,
@@ -61,6 +68,7 @@ export function Home() {
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
     reset()
   }
 
@@ -71,6 +79,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSbumitDisabeled = !task
